84b073c93e94cf76b8fafc5615c8fd8b91d32903
eatyu <yzjworking@163.com>
2019-06-11 20:18:53 +0800

    learn python is very interesting

---
+++ aa.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+print("sss")
+#  不需要转义
+a = r'不需要转义dasda/dsad/w/ads/wd/a\n'
+print(a)
+# 多行字符串
+b = r'''多行
+dasda
+aaaa'''
+print(b)
+# 布尔类型
+c = True
+print(c)
+# 空值
+d = None
+print(d)
+# 整型，python3没有Long，只有int
+e = 123456789012345678901234567890
+print(e)
+# 反复复制
+f = 'abc'
+print(f)
+f = 123456789
+print(f)
+f = None
+print(f)
+f = True
+print(f)
+# 多个变量赋值
+g = h = i = j = k = 100
+print(g)
