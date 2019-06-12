74bc7ce631d8c75fc4a9ee7fafd0d7c15214e3de
eatyu <yzjworking@163.com>
2019-06-12 22:54:19 +0800

    learn python is very interesting

---
+++ iflearn.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+java = 86
+python = 68
+
+if java > 80 and python > 80:
+    print('优秀')
+else:
+    print('不优秀')
+
+if (80 <= java < 90) or (python >= 80 and python < 90):
+    print('良好')
