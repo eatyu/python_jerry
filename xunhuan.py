f0a1b05d7b79e757e75e190f5ac825d0d05ce41f
eatyu <yzjworking@163.com>
2019-06-14 01:09:48 +0800

    learn for

--- xunhuan.py
+++ xunhuan.py
@@ -16,3 +16,26 @@ while count < 100:
     if b > 100:
         break
 print(b)
+
+print("\n\n\n\n########")
+
+for i in [1, 22, 3, 4, 500]:
+    print(i)
+else:
+    print("cool you have read all words")
+
+a = [1, 2, 3, 4, 5, 6, 7, 8]
+
+
+def search(names, special):
+    for name in names:
+        if name == special:
+            print("find")
+            break
+    else:
+        print("not found")
+
+
+firstname = ["tom", "tony", "zik"]
+search(firstname, "tom")
+search(firstname, "opop")
