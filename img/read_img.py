f68546694fa6b8c74091b89e6a71295206378274
eatyu <yzjworking@163.com>
2019-06-18 02:57:56 +0800

    learn for

---
+++ img/read_img.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+img=np.array(Image.open(r'C:\ProjectResourceDownload\crawler\zhihu\v2-a53f33209dc63eeb6c422a901d25da98_hd.jpg'))  #打开当前目录图像并转化为数字矩阵
+plt.figure("lena")
+plt.imshow(img)
+plt.axis('off')
+plt.show()
