3e67e71a22c48cda6df7bddc07250a176f1c194c
eatyu <yzjworking@163.com>
2019-06-17 20:35:57 +0800

    learn python is very interesting

---
+++ funlearn.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def aaa(a, b):
+    return a + b, a - b
+
+
+bb = aaa(7, 6)
+print(bb)
+
+dg1, dg2 = aaa(7, 6)
+print(dg1, end='============-----------........')
+print(dg2)
