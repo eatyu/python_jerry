d6b6c47fbc0321437925976d591b9340c5280663
eatyu <yzjworking@163.com>
2019-06-18 12:12:14 +0800

    learn python is very interesting

--- img/read_img.py
+++ img/read_img.py
@@ -2,9 +2,9 @@
 # -*- coding: utf-8 -*-
 
 
-from PIL import Image
-import numpy as np
-import matplotlib.pyplot as plt
+# from PIL import Image
+# import numpy as np
+# import matplotlib.pyplot as plt
 
 img_url = r'C:\ProjectResourceDownload\crawler\zhihu\v2-a53f33209dc63eeb6c422a901d25da98_hd.jpg'
 img = np.array(Image.open(img_url))  # 打开当前目录图像并转化为数字矩阵
